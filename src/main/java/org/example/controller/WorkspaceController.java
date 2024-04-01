dee1bc3e281e16bde8dafbd434ba851e2238823d
tedbeast <theodore.balashov@revature.com>
2024-04-01 01:11:24 -0400

    cors

--- src/main/java/org/example/controller/WorkspaceController.java
+++ src/main/java/org/example/controller/WorkspaceController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "true")
 public class WorkspaceController {
     WorkspaceService workspaceService;
     @Autowired
